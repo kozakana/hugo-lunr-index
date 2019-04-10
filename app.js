04caeb830b06ade8b8b066ebfd75e2121d028c55
kozakana <goki727@gmail.com>
2019-04-10 23:23:47 +0900

    fix promiss process

--- app.js
+++ app.js
@@ -3,4 +3,4 @@
 const index = require('./lib/index')
 const args = require('./lib/args')
 
-index.hugoLunrIndex(args.parse())
+index.hugoLunrIndex(args.parse()).then()
--- test/index.test.js
+++ test/index.test.js
@@ -2,16 +2,20 @@ const index = require('../lib/index')
 const lunr = require('lunr')
 const fs = require('fs')
 
-test('create index at default option', () => {
-  index.hugoLunrIndex()
-  const data = fs.readFileSync('lunr-index.json')
-  const idx = lunr.Index.load(JSON.parse(data))
-  expect(idx.search('open')[0].ref).toEqual('content/1-en.md')
+test('create index at default option', (done) => {
+  index.hugoLunrIndex().then(()=>{
+    const data = fs.readFileSync('lunr-index.json')
+    const idx = lunr.Index.load(JSON.parse(data))
+    expect(idx.search('open')[0].ref).toEqual('content/1-en.md')
+    done()
+  })
 })
 
-test('create index which supports Japanese', () => {
-  index.hugoLunrIndex({langs: 'ja'})
-  const data = fs.readFileSync('lunr-index.json')
-  const idx = lunr.Index.load(JSON.parse(data))
-  expect(idx.search('人')[0].ref).toEqual('content/1-ja.md')
+test('create index which supports Japanese', (done) => {
+  index.hugoLunrIndex({langs: 'ja'}).then(()=>{
+    const data = fs.readFileSync('lunr-index.json')
+    const idx = lunr.Index.load(JSON.parse(data))
+    expect(idx.search('人')[0].ref).toEqual('content/1-ja.md')
+    done()
+  })
 })
